9c00997026e79424b077733d22e50d0f543ae467
ramarfx <ramagamiing33@gmail.com>
2024-06-15 08:44:45 +0700

    http req & res

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,10 +1,56 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, HttpRedirectResponse, Param, Post, Query, Redirect, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller('api/users')
 export class UserController {
-  @Get()
-  get() {
-    return 'ini dari users';
+
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string>{
+    return {
+      'data': 'hello json'
+    }
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return  {
+      url: 'https://nestjs.com',
+      statusCode: 301
+    }
+  }
+
+  @Get('/udang')
+  @Redirect()
+  udang(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample',
+      statusCode: 301
+    }
+  }
+
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string
+  ): string {
+    return `get ${firstName} ${lastName}`
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `get ${id}`
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'get nestjs  ';
   }
 }
