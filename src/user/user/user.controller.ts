d46a9b29c23b42bf09e1cd2b070c1c4985a132eb
ramarfx <ramagamiing33@gmail.com>
2024-06-15 11:01:48 +0700

    view & cookies

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,8 +1,28 @@
 import { Controller, Get, Header, HttpCode, HttpRedirectResponse, Param, Post, Query, Redirect, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { title } from 'process';
 
 @Controller('api/users')
 export class UserController {
+
+  @Get('/view/hello')
+  viewHello(@Query('name') name: string, @Res() response: Response){
+    response.render('index.html', {
+      title: 'ini title',
+      name: name
+    })
+  }
+
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response){
+    response.cookie('name', name);
+    response.status(200).send('Success set cookie')
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name']
+  }
 
   @Get('/sample-response')
   @Header('Content-Type', 'application/json')
@@ -22,20 +42,11 @@ export class UserController {
     }
   }
 
-  @Get('/udang')
-  @Redirect()
-  udang(): HttpRedirectResponse {
-    return {
-      url: '/api/users/sample',
-      statusCode: 301
-    }
-  }
-
   @Get('/hello')
-  sayHello(
+   async sayHello(
     @Query('first_name') firstName: string,
     @Query('last_name') lastName: string
-  ): string {
+  ): Promise<string> {
     return `get ${firstName} ${lastName}`
   }
 
